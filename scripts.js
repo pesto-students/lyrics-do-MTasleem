719d24afa7949b9af74b7aa55008992eec35972a
Tasleem <tasleembca31@gmail.com>
2021-01-06 01:16:27 +0530

    add loader, add pagination functionalities

--- scripts.js
+++ scripts.js
@@ -2,11 +2,43 @@ var resultSet = [];
 const apiURL = 'https://api.lyrics.ovh';
 document.getElementsByClassName('prev')[0].setAttribute("disabled", true);
 document.getElementsByClassName('next')[0].setAttribute("disabled", true);
+
+document.getElementById("searchLyrics").addEventListener("click", searchLyrics);
+document.getElementById("next-btn").addEventListener("click", next);
+document.getElementById("prev-btn").addEventListener("click", prev);
+
 results.innerHTML = 'No Result';
 count = 0;
 lyricsResult = null;
-async function searchLyrics() {
-    let searchedParam = getName.value || null;
+
+const mainContainer = document.getElementsByClassName('main-container');
+
+var input = document.getElementById("myInput");
+input.addEventListener("keyup", function (event) {
+    if (event.keyCode === 13) {
+        event.preventDefault();
+        searchLyrics()
+    }
+});
+
+function showLoader() {
+    let ldsSpinner = document.createElement('div');
+    ldsSpinner.classList.add('lds-spinner');
+    for (let index = 0; index < 12; index++) {
+        let newElem = document.createElement('div');
+        ldsSpinner.appendChild(newElem);
+    }
+    document.body.appendChild(ldsSpinner);
+}
+
+function hideLoader() {
+    let ldsSpinner = document.body.getElementsByClassName('lds-spinner')[0];
+    document.body.removeChild(ldsSpinner);
+}
+
+async function searchLyrics(e) {
+    showLoader()
+    let searchedParam = myInput.value || null;
     if (searchedParam) {
         const res = await fetch(`${apiURL}/suggest/${searchedParam}`);
         const data = await res.json();
@@ -15,9 +47,10 @@ async function searchLyrics() {
         if (resultSet && resultSet.length) {
             document.getElementsByClassName('prev')[0].removeAttribute('disabled');
             document.getElementsByClassName('next')[0].removeAttribute('disabled');
-            document.getElementsByClassName('prev')[0].setAttribute(!count ? "disabled" : '', true)
+            document.getElementsByClassName('prev')[0].setAttribute("disabled", true)
         }
-        bindHTML(resultSet);
+        count = 1;
+        bindHTML(resultSet, count);
     } else {
         results.innerHTML = 'Enter artist or song name...';
         document.getElementsByClassName('prev')[0].setAttribute("disabled", true);
@@ -25,13 +58,19 @@ async function searchLyrics() {
     }
 }
 
-function bindHTML(resultSetVal) {
+function bindHTML(resultSetVal, count) {
     var output = "";
-    let resultSet = pagination(resultSetVal, count, 10)
+    let resultSet = pagination(resultSetVal, count, 4)
     for (let index = 0; index < resultSet.length; index++) {
-        output += '<section class="show-block"><section class="main-title" id="maintitle"><section class="wrap-main-title"><span class="name"><b>' + resultSet[index].artist.name + '</b></span> - <span class="title">' + resultSet[index].title + '</span></section><section class="show-lyrics"><button class="accordion" onclick="showLyrics(' + index + ')">Show Lyrics</button></section></section><section class="wrap-artist-list"></section></section>';
+        output += '<section class="show-block"><section class="main-title" id="maintitle"><section class="wrap-main-title"><section class="wrap-img"><img src="' + resultSet[index].album.cover_small + '" loading="lazy"/><section class="name"><b>' + resultSet[index].artist.name + '</b></section><section class="title1">' + resultSet[index].title + '</section><section class="audio-controls"><audio controls="controls" src="' + resultSet[index].preview + '">Your browser does not support the HTML5 audio element.</audio></section></section></section><section class="show-lyrics"><button class="btn-lyrics-search" onclick="showLyrics(' + index + ')">Show Lyrics</button></section></section><section class="wrap-artist-list"></section></section>';
     }
+
+    if (resultSet.length < 4) {
+        document.getElementsByClassName('next')[0].setAttribute("disabled", true);
+    }
+
     results.innerHTML = output;
+    hideLoader();
 }
 
 // Get lyrics for song
@@ -41,69 +80,35 @@ async function showLyrics(param) {
         let createEle = document.createElement('section');
         createEle.classList.add("artist-list");
         let artistList = document.getElementsByClassName('wrap-artist-list')[param];
+        artistList.innerText = ''
         const res = await fetch(`${apiURL}/v1/${getName[0].artist.name}/${getName[0].title}`);
         const data = await res.json();
-        createEle.innerHTML = data.lyrics || 'No data found';
+        // createEle.innerHTML = data.lyrics || 'No data found';
+        createEle.textContent += data.lyrics || 'No data found';
         artistList.appendChild(createEle);
     }
 }
-async function prev(param) {
+
+function prev(param) {
     count--;
+    if (count) {
+        showLoader()
+        document.getElementsByClassName('prev')[0].removeAttribute("disabled");
+        bindHTML(resultSet, count);
 
-    count++;
-    let prevPaginator = prevPaginator || lyricsResult.prev;
-    if (count && prevPaginator) {
-        document.getElementsByClassName('prev')[0].setAttribute(!count ? "disabled" : '', true);
-
-        // https://cors-anywhere.herokuapp.com/http://api.deezer.com/search?limit=15&q=stairway&index=15
-        let headers = new Headers();
-
-        headers.append('Content-Type', 'application/json');
-        headers.append('Accept', 'application/json');
-        headers.append('Access-Control-Allow-Origin', '*');
-        headers.append('Access-Control-Allow-Credentials', 'true');
-        headers.append('GET', 'POST', 'OPTIONS');
-
-        await fetch(`https://cors-anywhere.herokuapp.com/${prevPaginator}`, {
-            method: 'GET',
-            headers: headers
-        })
-            .then(response => response.text())
-            .then(resp => {
-                console.log(resp);
-                bindHTML(resp['data'])
-                prevPaginator = resp['prev'];
-            });
     } else {
         document.getElementsByClassName('prev')[0].setAttribute("disabled", true);
+        document.getElementsByClassName('next')[0].removeAttribute('disabled');
     }
-
 }
 
-async function next(param) {
+function next(param) {
     count++;
-    let nextPaginator = nextPaginator || lyricsResult.next;
     if (count) {
+        showLoader()
         document.getElementsByClassName('prev')[0].removeAttribute("disabled");
-        // https://cors-anywhere.herokuapp.com/http://api.deezer.com/search?limit=15&q=stairway&index=15
-        let headers = new Headers();
+        bindHTML(resultSet, count);
 
-        headers.append('Content-Type', 'application/json');
-        headers.append('Accept', 'application/json');
-        headers.append('Access-Control-Allow-Origin', '*');
-        headers.append('Access-Control-Allow-Credentials', 'true');
-        headers.append('GET', 'POST', 'OPTIONS');
-
-        await fetch(`https://cors-anywhere.herokuapp.com/${nextPaginator}`, {
-            method: 'GET',
-            headers: headers
-        })
-            .then(response => response.text())
-            .then(resp => {
-                console.log(resp);
-                bindHTML(resp['data'])
-                nextPaginator = resp['next'];
-            });
     } else {
         document.getElementsByClassName('prev')[0].setAttribute("disabled", true);
     }
@@ -121,3 +126,65 @@ function pagination(array, index, size) {
         return (n >= (index * size)) && (n < ((index + 1) * size))
     }))]
 }
+
+// async function prev(param) {
+//     count--;
+
+//     count++;
+//     let prevPaginator = prevPaginator || lyricsResult.prev;
+//     if (count && prevPaginator) {
+//         document.getElementsByClassName('prev')[0].setAttribute(!count ? "disabled" : '', true);
+
+//         // https://cors-anywhere.herokuapp.com/http://api.deezer.com/search?limit=15&q=stairway&index=15
+//         let headers = new Headers();
+
+//         headers.append('Content-Type', 'application/json');
+//         headers.append('Accept', 'application/json');
+//         headers.append('Access-Control-Allow-Origin', '*');
+//         headers.append('Access-Control-Allow-Credentials', 'true');
+//         headers.append('GET', 'POST', 'OPTIONS');
+
+//         await fetch(`https://cors-anywhere.herokuapp.com/${prevPaginator}`, {
+//             method: 'GET',
+//             headers: headers
+//         })
+//             .then(response => response.text())
+//             .then(resp => {
+//                 console.log(resp);
+//                 bindHTML(resp['data'])
+//                 prevPaginator = resp['prev'];
+//             });
+//     } else {
+//         document.getElementsByClassName('prev')[0].setAttribute("disabled", true);
+//     }
+// }
+
+
+// async function next(param) {
+//     count++;
+//     let nextPaginator = nextPaginator || lyricsResult.next;
+//     if (count) {
+//         document.getElementsByClassName('prev')[0].removeAttribute("disabled");
+//         // https://cors-anywhere.herokuapp.com/http://api.deezer.com/search?limit=15&q=stairway&index=15
+//         let headers = new Headers();
+
+//         headers.append('Content-Type', 'application/json');
+//         headers.append('Accept', 'application/json');
+//         headers.append('Access-Control-Allow-Origin', '*');
+//         headers.append('Access-Control-Allow-Credentials', 'true');
+//         headers.append('GET', 'POST', 'OPTIONS');
+
+//         await fetch(`https://cors-anywhere.herokuapp.com/${nextPaginator}`, {
+//             method: 'GET',
+//             headers: headers
+//         })
+//             .then(response => response.text())
+//             .then(resp => {
+//                 console.log(resp);
+//                 bindHTML(resp['data'])
+//                 nextPaginator = resp['next'];
+//             });
+//     } else {
+//         document.getElementsByClassName('prev')[0].setAttribute("disabled", true);
+//     }
+// }
